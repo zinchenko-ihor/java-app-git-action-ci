613c4eb8c1e2dcc4825e356a9e1d3a579c74feb3
Ihor Zinchenko <Igor.Zinchenko@fuib.com>
2026-05-22 07:22:09 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Github Action. Continuous Integration
---
+++ src/main/java/com/edu/Application.java
@@ -0,0 +1,18 @@
+package com.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
